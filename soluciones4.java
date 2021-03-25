70696e0943ee386cc667a118ed34dfbefd297a8c
FelipeDemarchi2004 <81364233+FelipeDemarchi2004@users.noreply.github.com>
2021-03-25 10:24:11 -0300

    Add files via upload

---
+++ soluciones4.java
@@ -0,0 +1,25 @@
+package soluciones.informaticas;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc09
+ */
+public class soluciones4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Deberan armar un programa que ingrese por teclado la cantidad de horas trabajadas y el costo de las mismas y calcule el sueldo por mes y por año
+    
+
+    
+}
+    
+}
