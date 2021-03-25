f530c7352c070007cfe6091b24ffdce9fd5bb510
FelipeDemarchi2004 <81364233+FelipeDemarchi2004@users.noreply.github.com>
2021-03-25 09:30:04 -0300

    Add files via upload

---
+++ SolucionesInformaticas.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soluciones.informaticas;
+import java.util.Scanner;
+/**
+ *
+ * @author pc09
+ */
+public class SolucionesInformaticas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int a =4;
+       int b =7;
+       int res;
+       res=a+b;
+       System.out.println("El resultado es: "+res);
+        
+    }
+    
+}
