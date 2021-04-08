496dd5ea28b5224c7aa85f3bed8a5e779ef0a389
FelipeDemarchi2004 <81364233+FelipeDemarchi2004@users.noreply.github.com>
2021-04-08 08:42:24 -0300

    Add files via upload

---
+++ soluciones5.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soluciones.informaticas;
+import java.util.Scanner;
+/**
+ *
+ * @author pc09
+ */
+public class soluciones5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  
+        Scanner entrada=new Scanner(System.in);
+        int numero;
+        System.out.print("introduce numero entero");
+        numero=entrada.nextInt();
+        if(numero%2==0){
+        System.out.print("par");
+        }
+        else
+            System.out.print("imapar");
+            }
+    }
+    
+
